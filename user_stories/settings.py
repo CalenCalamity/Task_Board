9a057a80e0bce01f9266732f2f1d137e4d91648a
Lance Mac Donald <lancemac26@gmail.com>
2021-06-20 18:39:27 +0200

    Update: * Added in postgresql

--- user_stories/settings.py
+++ user_stories/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'user_stories.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'user_stories',
+        'USER': 'postgres',
+        'PASSWORD': 'karrimore26',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
